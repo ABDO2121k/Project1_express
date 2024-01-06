d7d532bc3b1fb76268e9b3358bd802ea139c01e1
ABDO2121k <113726842+ABDO2121k@users.noreply.github.com>
2024-01-06 20:48:19 +0000

    Update index.js

--- pr/api/index.js
+++ pr/api/index.js
@@ -12,14 +12,6 @@ app.use(cors({origin:"https://blog-client-cn05.onrender.com",credentials:true}))
 app.use(cookieParser("hhhhabdo"));  // ad cookie parser hia li kat5lina njibo l cookie mn session
 
 
-app.use(express.static(path.join(process.cwd(), 'client/build')));
-
-// Handle GET requests to all non-API routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'client/build', 'index.html'));
-});
-
-
 app.use("/api/posts",postRoutes)
 app.use("/api/users",userRoutes)
 app.use("/api/auth",authRoutes)
