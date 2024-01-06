ee01b743a0a80685d5709bc895ca8c9bf357e8e6
ABDO2121k <113726842+ABDO2121k@users.noreply.github.com>
2024-01-06 17:19:46 +0100

    Update db.js

--- pr/api/db.js
+++ pr/api/db.js
@@ -1,9 +1,17 @@
 import mysql from 'mysql2'
 
+// export const db=mysql.createConnection({
+//     host:"localhost",
+//     database:"blog",
+//     user:"root",
+//     password:"Abcd1234@Kouah",
+// })
+
 export const db=mysql.createConnection({
-    host:"localhost",
-    database:"blog",
-    user:"root",
-    password:"Abcd1234@Kouah",
+    host:"bwiftawta8bdzwctqjzw-mysql.services.clever-cloud.com",
+    database:"bwiftawta8bdzwctqjzw",
+    user:"urjgf4nkrl4u5gtb",
+    password:"GW0NPqm5dwQ5K5WtH2ow",
 })
+
 
